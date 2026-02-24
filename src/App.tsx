89142421b09b3bec060bdcab550982b32b3f1be5
fatiseddigh <seddighfati@gmail.com>
2026-02-24 12:45:52 +0400

    chore: setup tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,9 @@
-import "./App.css";
-
 function App() {
-  return <>helllo</>;
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-gray-100">
+      <h1 className="text-3xl font-bold text-blue-600 ">Project Initialized</h1>
+    </div>
+  );
 }
 
 export default App;
